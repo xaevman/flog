8c56ce049bc5e72ddb749c8a71fd3ef161e9c93e
Jared Chavez <jared@undeadlabs.net>
2018-10-18 11:20:52 -0700

    fix a bug when closing a disabled file-backed log

--- BufferedLog.go
+++ BufferedLog.go
@@ -2,7 +2,7 @@
 //
 //  BufferedLog.go
 //
-//  Copyright (c) 2014, Jared Chavez. 
+//  Copyright (c) 2014, Jared Chavez.
 //  All rights reserved.
 //
 //  Use of this source code is governed by a BSD-style
@@ -13,153 +13,152 @@
 package flog
 
 import (
-    "bytes"
-    "io"
-    "log"
-    "os"
-    "sync"
-    "sync/atomic"
-    "time"
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"sync"
+	"sync/atomic"
+	"time"
 )
 
 // BufferedLog represents a buffered, file-backed logger and enforces a standardized
 // logging format. New logging entries are sent to a memory buffer and
-// periodically flushed to the backing file at configurable intervals 
+// periodically flushed to the backing file at configurable intervals
 // by a seperate goroutine.
 type BufferedLog struct {
-    baseDir  string
-    buffer   bytes.Buffer
-    chClose  chan interface{}
-    enabled  int32
-    file     *os.File
-    flushSec int32
-    lock     sync.RWMutex
-    logger   *log.Logger
-    name     string
+	baseDir   string
+	buffer    bytes.Buffer
+	chClose   chan interface{}
+	enabled   int32
+	hasClosed bool
+	file      *os.File
+	flushSec  int32
+	lock      sync.RWMutex
+	logger    *log.Logger
+	name      string
 }
 
 // BaseDir returns the base directory of the file backing this BufferedLog instance.
 func (this *BufferedLog) BaseDir() string {
-    this.lock.RLock()
-    defer this.lock.RUnlock()
+	this.lock.RLock()
+	defer this.lock.RUnlock()
 
-    return this.baseDir
+	return this.baseDir
 }
 
 // Close disables the BufferedLog instance, flushes any remaining entries to disk, and
 // then closes the backing log file.
 func (this *BufferedLog) Close() {
-    this.lock.Lock()
-    defer this.lock.Unlock()
+	this.lock.Lock()
+	defer this.lock.Unlock()
 
-    this.enabled = 0
+	this.enabled = 0
 
-    this.print("==== Close log ====")
+	this.print("==== Close log ====")
 
-    // stop flush routine
-    this.chClose <- nil
-    <-this.chClose
+	// stop flush routine
+	this.chClose <- nil
+	<-this.chClose
 
-    // flush logs
-    this.flushLogs()
+	// flush logs
+	this.flushLogs()
 
-    // close file
-    this.file.Close()
+	// close file
+	this.file.Close()
 }
 
 // Disable temporarily disables the BufferedLog instance. New calls to Print will have no
 // effect.
 func (this *BufferedLog) Disable() {
-    atomic.StoreInt32(&this.enabled, 0)
+	atomic.StoreInt32(&this.enabled, 0)
 }
 
 // Enable re-enables an BufferedLog instance.
 func (this *BufferedLog) Enable() {
-    atomic.StoreInt32(&this.enabled, 1)
+	atomic.StoreInt32(&this.enabled, 1)
 }
 
 // FlushInterval returns the interval between log flushes in seconds.
 func (this *BufferedLog) FlushIntervalSec() int32 {
-    return atomic.LoadInt32(&this.flushSec)
+	return atomic.LoadInt32(&this.flushSec)
 }
 
-// Name returns the friendly name of the log. 
+// Name returns the friendly name of the log.
 func (this *BufferedLog) Name() string {
-    this.lock.RLock()
-    defer this.lock.RUnlock()
+	this.lock.RLock()
+	defer this.lock.RUnlock()
 
-    return this.name
+	return this.name
 }
 
 // Print formats and buffers a new log entry as long as the BufferedLog instance
 // is enabled.
 func (this *BufferedLog) Print(format string, v ...interface{}) {
-    this.lock.RLock()
-    defer this.lock.RUnlock()
+	this.lock.RLock()
+	defer this.lock.RUnlock()
 
-    if atomic.LoadInt32(&this.enabled) < 1 {
-        return
-    }
+	if atomic.LoadInt32(&this.enabled) < 1 {
+		return
+	}
 
-    if v == nil {
-        this.print(format)
-    } else {
-        this.print(format, v...)
-    }
+	if v == nil {
+		this.print(format)
+	} else {
+		this.print(format, v...)
+	}
 }
 
 // SetFlushIntervalSec sets the interval at which the log buffer worker
 // will attempt to flush new entries into the backing log file.
 func (this *BufferedLog) SetFlushIntervalSec(interval int32) {
-    atomic.StoreInt32(&this.flushSec, interval)
+	atomic.StoreInt32(&this.flushSec, interval)
 }
 
-// asyncFlush is run in a separate goroutine and periodically flushes 
+// asyncFlush is run in a separate goroutine and periodically flushes
 // buffered entries to the backing file.
 func (this *BufferedLog) asyncFlush() {
-    run := true
+	run := true
 
-    for run {
-        flushSec := atomic.LoadInt32(&this.flushSec)
+	for run {
+		flushSec := atomic.LoadInt32(&this.flushSec)
 
-        select {
-            case <-this.chClose:
-                run = false
-                this.print("Async log shutdown")
-                continue
-            case <-time.After(time.Duration(flushSec) * time.Second):
-                this.flushLogs()
-        }
-    }
+		select {
+		case <-this.chClose:
+			run = false
+			this.print("Async log shutdown")
+			continue
+		case <-time.After(time.Duration(flushSec) * time.Second):
+			this.flushLogs()
+		}
+	}
 
-    this.chClose <- nil
+	this.chClose <- nil
 }
 
 // flushLogs copies the contents of the log buffer into the open log file.
 func (this *BufferedLog) flushLogs() {
-    _, err := io.Copy(this.file, &this.buffer)
-    if err != nil {
-        panic(err)
-    }
+	_, err := io.Copy(this.file, &this.buffer)
+	if err != nil {
+		panic(err)
+	}
 
-    err = this.file.Sync()
-    if err != nil {
-        panic(err)
-    }
+	err = this.file.Sync()
+	if err != nil {
+		panic(err)
+	}
 }
 
 // print performs log entry formatting and uses a std library logger to
 // write the formatted entry into the BufferedLog's buffer.
 func (this *BufferedLog) print(format string, v ...interface{}) {
-    format = fixFormat(this.name, format)
+	format = fixFormat(this.name, format)
 
-    if v == nil {
-        log.Print(format)
-        this.logger.Print(format)
-    } else {
-        log.Printf(format, v...)
-        this.logger.Printf(format, v...)
-    }
+	if v == nil {
+		log.Print(format)
+		this.logger.Print(format)
+	} else {
+		log.Printf(format, v...)
+		this.logger.Printf(format, v...)
+	}
 }
-
-
